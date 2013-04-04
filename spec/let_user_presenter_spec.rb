218f934bce0a4afd9d7214f4413316c078dc0bb3
Ben Orenstein <ben.orenstein@gmail.com>
2013-04-04 18:02:56 -0400

    Add let example

---
+++ spec/let_user_presenter_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe UserPresenter do
+  let(:user) do
+    stub('user', name: 'Lord Bennington',
+        company: stub('company', name: 'Katzenjammers'))
+  end
+
+  describe '#name' do
+    it "returns the user's name" do
+      UserPresenter.new(user).name.should == 'Lord Bennington'
+    end
+  end
+
+  describe '#parent_company_name' do
+    it "returns the user's company name" do
+      UserPresenter.new(user).company_name.should ==
+        'Katzenjammers'
+    end
+  end
+end
