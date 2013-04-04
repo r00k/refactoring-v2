a43fe3650abacaf29ae7982eed70755be2b3a959
Ben Orenstein <ben.orenstein@gmail.com>
2013-04-04 13:46:12 -0400

    Bring in active_support. Move to cattr_writer on ApplicationController

--- lib/application_controller.rb
+++ lib/application_controller.rb
@@ -1,5 +1,15 @@
+require 'active_support/core_ext/class/attribute_accessors'
+require 'kissmetrics_http_client'
+
 class ApplicationController
+  cattr_writer :kissmetrics_http_client
+
+  KISSMETRICS_API_KEY = 'lolapikey'
+
   def kissmetrics_http_client
-    "lol I'm a client"
+    @@kissmetrics_http_client.new(KISSMETRICS_API_KEY)
   end
 end
+
+# In config/initializers/kissmetrics_http_client.rb
+ApplicationController.kissmetrics_http_client = KissmetricsHttpClient
---
+++ lib/kissmetrics_http_client.rb
@@ -0,0 +1,5 @@
+class KissmetricsHttpClient
+  def initialize(api_key)
+    "LOL I'M A CLIENT!"
+  end
+end
